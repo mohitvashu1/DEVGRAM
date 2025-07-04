df177d78118d738aaf406beecf2a44a67faf442b
mohitvashu1 <mohitchoubeybca@gmail.com>
2025-07-04 14:16:15 +0530

    Playing with routes handelers

--- Src/app.js
+++ Src/app.js
@@ -1,13 +1,25 @@
 const express =require('express');
 const app =express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
 
-app.get('/help', (req, res) => {
-  res.send('Hello World help')
-})
+app.use('/help', (req, res, next) => {
+  // res.send('Hello World 1');
+  next();
+},
+ (req, res,next) => {
+  res.send('Hello World 2');
+  next();
+},
+ (req, res) => {
+  res.send('Hello World 3')
+}
+)
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World')
+// })
+
+
 
 
 app.listen(3000,()=>{
